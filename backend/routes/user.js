224528710c53eeb044f2e97de32efad696821d0f
Akash Gupta <akash1128355@gmail.com>
2023-11-20 23:29:03 +0530

    error handled

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,7 +8,6 @@ const router=express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/follow/:id").get(isAuthenticated ,followUser);
 
 router.route("/update/password").put(isAuthenticated,updatePassword);
 router.route("/update/profile").put(isAuthenticated,updateProfile);
@@ -17,9 +16,6 @@ router.route("/delete/me").delete(isAuthenticated,deleteProfile);
 
 router.route("/me").get(isAuthenticated,myProfile);
 
-router.route("/my/posts").get(isAuthenticated,getMyPosts);
-
-router.route("/userposts/:id").get(isAuthenticated,getUserPosts)
 router.route("/user/:id").get(isAuthenticated,getUserProfile);
 router.route("/users").get(isAuthenticated,getAllUsers);
 
