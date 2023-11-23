c6d017df1f7d5606f0cfa48a440d7197a5d7a2f6
Ajit Kumar Maurya <kumarmauryaajit@gmail.com>
2023-11-23 21:45:41 +0530

    Minor fixes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,19 @@
 const express=require('express');
-const { register, login, followUser, logout, updatePassword, updateProfile, deleteProfile, myProfile, getUserProfile, getAllUsers, forgotPassword, resetPassword, getMyPosts, getUserPosts, createQuiz, getUserQuizzes } = require('../controllers/user');
+const { 
+    register, 
+    login, 
+    logout, 
+    updatePassword, 
+    updateProfile, 
+    deleteProfile, 
+    myProfile, 
+    getUserProfile, 
+    getAllUsers, 
+    forgotPassword, 
+    resetPassword, 
+    createQuiz, 
+    getUserQuizzes 
+} = require('../controllers/user');
 const {isAuthenticated}=require("../middlewares/auth");
 const { checkAdmin } = require('../middlewares/checkAdmin');
 const { createGroup } = require('../controllers/user');
@@ -8,7 +22,6 @@ const router=express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/follow/:id").get(isAuthenticated ,followUser);
 
 router.route("/update/password").put(isAuthenticated,updatePassword);
 router.route("/update/profile").put(isAuthenticated,updateProfile);
@@ -17,9 +30,6 @@ router.route("/delete/me").delete(isAuthenticated,deleteProfile);
 
 router.route("/me").get(isAuthenticated,myProfile);
 
-router.route("/my/posts").get(isAuthenticated,getMyPosts);
-
-router.route("/userposts/:id").get(isAuthenticated,getUserPosts)
 router.route("/user/:id").get(isAuthenticated,getUserProfile);
 router.route("/users").get(isAuthenticated,getAllUsers);
 
