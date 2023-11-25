20c22cefef6dba39408c3967d0baa7542bec82a2
Ajit Kumar Maurya <kumarmauryaajit@gmail.com>
2023-11-25 07:01:57 +0530

    Minor fixes

--- frontend/src/Components/UpdateProfile/UpdateProfile.jsx
+++ frontend/src/Components/UpdateProfile/UpdateProfile.jsx
@@ -52,7 +52,7 @@ const UpdateProfile = () => {
     <div className="updateProfile">
       <form className="updateProfileForm" onSubmit={submitHandler}>
         <Typography variant="h3" style={{ padding: "2vmax" }}>
-          Social Aap
+          inQUIZitive
         </Typography>
 
         <Avatar
